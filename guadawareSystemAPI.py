4d9c3eac015dbea35d7d72c4d3ff0db15a4e680a
LTS-N <labtecsev.n@gmail.com>
2026-08-23 13:21:23 +0200

    added airplane mode

--- guadawareSystemAPI.py
+++ guadawareSystemAPI.py
@@ -15,4 +15,11 @@ def sh(cmd):
 def get_statusbaruser():
     return "<DOCTYPE html><html><head><style>body { color: #000000; font-size: clamp(11px, 1.6vw, 14px); text-shadow: 0 1px 2px rgba(0, 0, 0, 0.4); font-family: -apple-system, \"Helvetica Neue\", Helvetica, Arial, sans-serif; }</style></head><body>" + subprocess.run(["whoami"], shell=True, capture_output=True, text=True).stdout + "</body></html>"
 
+@route("/setAirplanemode/<switch>")
+def set_airplanemode(switch):
+    if switch=="0":
+        subprocess.run(["nmcli", "radio", "all", "on"])
+    elif switch=="1":
+        subprocess.run(["nmcli", "radio", "all", "off"])
+    
 runapi(host="localhost", port=8080, debug=True)
