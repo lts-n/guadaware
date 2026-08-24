4dc1d98b52352f6335bce34a62ed987dbfe6a4e6
Leonardoghurt <leogavhur@gmail.com>
2026-08-24 18:33:27 +0200

    removed unused getStatusbarUser endpoint and added getPCModel function

--- guadawareSystemAPI.py
+++ guadawareSystemAPI.py
@@ -15,10 +15,6 @@ def sh(cmd):
     result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
     return result.stdout + result.stderr
 
-@route("/getStatusbarUser")
-def get_statusbaruser():
-    return "<DOCTYPE html><html><head><style>body { color: #000000; font-size: clamp(11px, 1.6vw, 14px); text-shadow: 0 1px 2px rgba(0, 0, 0, 0.4); font-family: -apple-system, \"Helvetica Neue\", Helvetica, Arial, sans-serif; }</style></head><body>" + subprocess.run(["whoami"], shell=True, capture_output=True, text=True).stdout + "</body></html>"
-
 @route("/setAirplanemode/<switch>")
 def set_airplanemode(switch):
     if switch=="0":
@@ -36,6 +32,11 @@ def get_diskusage():
     result = subprocess.run("df -h / | awk 'NR==2 {print $3 \"/\" $2}'", shell=True, capture_output=True, text=True)
     return result.stdout.strip()
 
+@route("/getPCModel")
+def get_pc_model():
+    result = subprocess.run("cat /sys/devices/virtual/dmi/id/product_name", shell=True, capture_output=True, text=True)
+    return result.stdout.strip()
+
 @route("/getCPUModel")
 def get_cpu_model():
     result = subprocess.run("lscpu | awk -F: '/Model name/ {print $2}'", shell=True, capture_output=True, text=True)
@@ -45,4 +46,5 @@ def get_cpu_model():
 def get_gpu_model():
     result = subprocess.run("lspci | grep -i 'vga\|3d\|2d' | awk -F: '{print $3}'", shell=True, capture_output=True, text=True)
     return result.stdout.strip()
+
 runapi(host="localhost", port=8080, debug=True)
